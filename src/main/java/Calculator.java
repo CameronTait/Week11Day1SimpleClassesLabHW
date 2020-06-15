ad79b4ff561690b33a2d2c35753bbbcc597c0eb1
CameronTait <cameron-tait@hotmail.com>
2020-06-15 16:31:51 +0100

    added functions for calculator.  attempted testing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,13 @@
 public class Calculator {
+
+    public Calculator() {
+    }
+    
+    public int Add(int num1, int num2) { return this.num1 + this.num2 }
+    
+    public int Subtract(int num1, int num2) { return this.num1 - this.num2 }
+    
+    public int Multiply(int num1, int num2) { return this.num1 * this.num2 }
+
+    public double Divide(double num1, double num2) { return this.num1 / this.num2 }
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest{
+    @Test
+    public void Add(){
+        Calculator calculator;
+        @Before
+        public void before() { calculator = new Calculator(); }
+
+
+    }
+}
